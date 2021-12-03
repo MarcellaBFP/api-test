8662a5ce5a88c1cf31df11ce85fe6d78d297573e
Marcella <marcellabupiresn@gmail.com>
2021-12-02 22:52:05 -0300

    update

--- src/modules/equipaments/routes/equipaments.routs.ts
+++ src/modules/equipaments/routes/equipaments.routs.ts
@@ -1,13 +1,58 @@
 import { Router } from "express";
 import EquipamentsController from "../controllers/EquipamentsController";
+import{celebrate, Joi, Segments} from 'celebrate'
 
 const equipamentsRouter =Router();
 const equipamentsController = new EquipamentsController();
 
 equipamentsRouter.get(' / ',  equipamentsController.index);
-equipamentsRouter.get(' /:id', equipamentsController.show);
-equipamentsRouter.post("/", equipamentsController.create);
-equipamentsRouter.put('/:id', equipamentsController.update);
-equipamentsRouter.delete('/:id', equipamentsController.delete);
+
+equipamentsRouter.get(
+    '/:id', 
+ celebrate({
+     [Segments.PARAMS]: {
+         id:  Joi.string().uuid().required(),
+     },
+ }),
+ equipamentsController.show,
+ );
+
+equipamentsRouter.post(
+    "/", 
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            serial_number: Joi.string().required(),
+
+        },
+    }),
+    equipamentsController.create
+    );
+
+equipamentsRouter.put(
+'/:id', 
+celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            serial_number: Joi.string().required(),
+
+        },
+     [Segments.PARAMS]: {
+         id:  Joi.string().uuid().required(),
+     },
+    }),
+
+equipamentsController.update
+);
+
+equipamentsRouter.delete(
+'/:id', 
+celebrate({
+     [Segments.PARAMS]: {
+         id:  Joi.string().uuid().required(),
+     },
+ }),
+equipamentsController.delete
+);
 
 export default equipamentsRouter;
