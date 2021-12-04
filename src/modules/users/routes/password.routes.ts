446e917c1e70fc71846bbdbb2f5181bc85bba4a8
Marcella <marcellabupiresn@gmail.com>
2021-12-04 18:44:41 -0300

    update

--- src/modules/users/routes/password.routes.ts
+++ src/modules/users/routes/password.routes.ts
@@ -1,15 +1,28 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
 import ForgotPasswordController from '../controllers/ForgotPasswordController';
+import ResetPasswordController from '../controllers/ResetPasswordController';
 
 const passwordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
 
 passwordRouter.post(
   '/forgot',
   celebrate({
     [Segments.BODY]: {
       email: Joi.string().email().required(),
+    },
+  }),
+  forgotPasswordController.create,
+);
+passwordRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().email().required(),
+      password: Joi.string().required(), 
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
     },
   }),
   forgotPasswordController.create,
