84b93ffe45c9fe1d8ff8464eaaa312802e779784
Marcella <marcellabupiresn@gmail.com>
2021-12-04 00:40:22 -0300

    update

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import equipamentsRouter from '@modules/equipaments/routes/equipaments.routs';
 import usersRouter from '@modules/users/routes/users.routes';
+import sessionsRouter from '@modules/users/routes/sessions.routes';
 
 const routes = Router();
 
 routes.use('/equipaments', equipamentsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
