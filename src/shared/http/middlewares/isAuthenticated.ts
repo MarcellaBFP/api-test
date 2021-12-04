bac5d7caebba36ae58b61c4430d6a5ecda57e8ac
Marcella <marcellabupiresn@gmail.com>
2021-12-04 13:29:06 -0300

    update

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,6 +3,12 @@ import { verify } from "jsonwebtoken";
 import { NextFunction, request, Request, Response } from "express";
 import authConfig from  '@config/auth';
 
+interface TokenPayload{
+    iat: number;
+    exp: number;
+    sub: string;
+}
+
 export default function isAuthenticated(
     request: Request,
     response: Response,
@@ -16,8 +22,14 @@ export default function isAuthenticated(
     const [, token]  =  authHeader.split(' ');
 
     try {
-        const decodeToken =  verify(token, authConfig.jwt.secret);
+        const decodedToken =  verify(token, authConfig.jwt.secret);
 
+
+         const{ sub } = decodedToken as TokenPayload;
+      
+         request.user = {
+             id: sub,
+         }
         return next();
     } catch {
         throw new AppError('Invalid JWT Token.');       
