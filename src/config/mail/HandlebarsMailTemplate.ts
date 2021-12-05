bd7709012cfd2eb8f35223c778aae648455e6af7
Marcella <marcellabupiresn@gmail.com>
2021-12-04 22:04:30 -0300

    update

--- src/config/mail/HandlebarsMailTemplate.ts
+++ src/config/mail/HandlebarsMailTemplate.ts
@@ -1,20 +1,25 @@
 import handlebars from 'handlebars';
+import fs from 'fs';
 
 interface ITemplateVariable {
     [key: string]: string | number;
 }
 interface IParseMailTemplate {
-    template: string;
+    file: string;
     variables: ITemplateVariable;
 }
 
 export default class handlebarsMailTemplate {
     public async parse({
-        template, 
+        file, 
         variables,
     }:  IParseMailTemplate): Promise<string> {
+    
+    const templateFileContent = await fs.promises.readFile(file,{
+        encoding: 'utf-8',
 
-    const parseTemplate = handlebars.compile(template);
+    })
+    const parseTemplate = handlebars.compile(file);
 
     return parseTemplate(variables);
 
