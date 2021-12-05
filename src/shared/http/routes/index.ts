6355db98cad38fbd694ed390889ac3660dc0e25f
Marcella <marcellabupiresn@gmail.com>
2021-12-04 22:45:29 -0300

    MIgration Customers

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,8 @@ import equipamentsRouter from '@modules/equipaments/routes/equipaments.routs';
 import usersRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
+import { profile } from 'console';
+import profileRouter from '@modules/equipaments/routes/profile.routes';
 
 const routes = Router();
 
@@ -10,5 +12,6 @@ routes.use('/equipaments', equipamentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
